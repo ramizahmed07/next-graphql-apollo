034efb19c669ada5c160f2722ae0033741aac5b6
Ramiz Ahmed <ramizahmedar@gmail.com>
2021-06-10 04:09:57 +0500

    add header, nav

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b293010aace527feec32";
+/******/ 	var hotCurrentHash = "fc303f6769cf2bcbce95";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,5 +1,8 @@
+import Header from "./Header";
+
 const Page = ({ children }) => (
   <div>
+    <Header />
     <h2>I am page component</h2>
     {children}
   </div>
