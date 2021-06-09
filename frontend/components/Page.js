7e1219e55b3f0f2e7be6c4c58cfbfc9d23b451ea
Ramiz Ahmed <ramizahmedar@gmail.com>
2021-06-10 03:51:56 +0500

    add  next js global files

---
+++ frontend/components/Page.js
@@ -0,0 +1,8 @@
+const Page = ({ children }) => (
+  <div>
+    <h2>I am page component</h2>
+    {children}
+  </div>
+);
+
+export default Page;
---
+++ frontend/pages/_app.js
@@ -0,0 +1,11 @@
+import Page from "../components/Page";
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
+};
+
+export default MyApp;
